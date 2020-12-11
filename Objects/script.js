ed821fac6a5f37634f1a566d99217b06c2003fc2
kimpavel1993 <kimpavel1993@mail.ru>
2020-12-12 06:55:22 +0700

    fix task Object

--- Objects/script.js
+++ Objects/script.js
@@ -1,108 +1,104 @@
 "use strict";
 
 (function () {
-        function Country(name) {
-            this.name = name;
-        }
-
-        // noinspection ES6ConvertVarToLetConst
-        var russia = new Country("Россия")
-        // noinspection ES6ConvertVarToLetConst
-        var japan = new Country("Япония")
-        // noinspection ES6ConvertVarToLetConst
-        var france = new Country("Франция")
-
-        // noinspection ES6ConvertVarToLetConst
-        var countries = [russia, japan, france];
-
-        russia.city = [
-            {
-                name: "Москва",
-                population: 12678079
-            },
-            {
-                name: "Новосибирск",
-                population: 1625631
-            },
-            {
-                name: "Санкт-Петербург",
-                population: 5398064
-            }
-        ];
-
-        japan.city = [
-            {
-                name: "Токио",
-                population: 14002973
-            },
-            {
-                name: "Киото",
-                population: 1463444
-            },
-            {
-                name: "Осака",
-                population: 2717953
-            }
-        ];
-
-        france.city = [
-            {
-                name: "Париж",
-                population: 2148327
-            },
-            {
-                name: "Марсель",
-                population: 861635
-            },
-        ];
-
-        console.log(countries);
-
-        // noinspection ES6ConvertVarToLetConst
-        var maxCitiesCount = 0;
-        // noinspection ES6ConvertVarToLetConst
-        var maxCitiesCountry = Object.create(countries).filter(function (e) {
-                if (e.city.length > maxCitiesCount) {
-                    maxCitiesCount = e.city.length;
+    // noinspection ES6ConvertVarToLetConst
+    var countries = [
+        {
+            name: "Россия",
+            cities: [
+                {
+                    name: "Москва",
+                    population: 12678079
+                },
+                {
+                    name: "Новосибирск",
+                    population: 1625631
+                },
+                {
+                    name: "Санкт-Петербург",
+                    population: 5398064
                 }
+            ]
+        },
+        {
+            name: "Япония",
+            cities: [
+                {
+                    name: "Токио",
+                    population: 14002973
+                },
+                {
+                    name: "Киото",
+                    population: 1463444
+                },
+                {
+                    name: "Осака",
+                    population: 2717953
+                }
+            ]
+        },
+        {
+            name: "Франция",
+            cities: [
+                {
+                    name: "Париж",
+                    population: 2148327
+                },
+                {
+                    name: "Марсель",
+                    population: 861635
+                },
+            ]
+        }
+    ];
 
-                return e.city.length === maxCitiesCount;
+    console.log(countries);
+
+    function getMaxCitiesCountry() {
+
+        // noinspection ES6ConvertVarToLetConst
+        var maxCitiesCount = 0
+
+        // noinspection ES6ConvertVarToLetConst
+        countries.forEach(function (e) {
+            if (maxCitiesCount < e.cities.length) {
+                maxCitiesCount = e.cities.length
             }
-        )
+        })
+
+        // noinspection ES6ConvertVarToLetConst
+        var maxCitiesCountry = countries.filter(function (e) {
+            return e.cities.length >= maxCitiesCount;
+        })
 
         console.log("Страны, с максимальным количеством городов", maxCitiesCountry);
+    }
 
-        // noinspection ES6ConvertVarToLetConst
-        var russiaPopulation = Object.create(russia.city).reduce(function (sum, e) {
-            return sum + e.population;
-        }, 0);
+    getMaxCitiesCountry();
 
-        // noinspection ES6ConvertVarToLetConst
-        var japanPopulation = Object.create(japan.city).reduce(function (sum, e) {
-            return sum + e.population;
-        }, 0);
+    function getCountriesWithSumPopulation() {
+        countries.forEach(function (e) {
+            // noinspection ES6ConvertVarToLetConst
+            var count = 0;
 
-        // noinspection ES6ConvertVarToLetConst
-        var francePopulation = Object.create(france.city).reduce(function (sum, e) {
-            return sum + e.population;
-        }, 0);
+            e.cities.forEach(function (e) {
+                count += e.population;
 
-        // noinspection ES6ConvertVarToLetConst
-        var sumPopulations = [russiaPopulation, japanPopulation, francePopulation];
+            })
 
-        // noinspection ES6ConvertVarToLetConst
-        var myCountries = countries.map(function (e) {
-            return e.name
+            e.sumPopulation = count;
         });
 
         // noinspection ES6ConvertVarToLetConst
-        var result = Object.create(myCountries).reduce(function (e, value, i ) {
-            e[value] = sumPopulations[i];
+        var result = countries.reduce(function (e, value) {
+            // noinspection JSUnresolvedVariable
+            e[value.name] = value.sumPopulation;
 
             return e;
-        }, {})
+        }, {});
 
         console.log(result);
     }
-)
-()
+
+    getCountriesWithSumPopulation();
+})();
