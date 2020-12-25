13931bc9c947c0b2d370ab3db5e481b4ba4618ed
kimpavel1993 <kimpavel1993@mail.ru>
2020-12-25 15:19:06 +0700

    fix task PhoneBook jQuery

--- Phonebook_jQuery/script.js
+++ Phonebook_jQuery/script.js
@@ -1,57 +1,46 @@
 $(document).ready(function () {
-    // noinspection ES6ConvertVarToLetConst
     var secondNameTextField = $("#second-name-text-field");
-    // noinspection ES6ConvertVarToLetConst
     var firstNameTextField = $("#first-name-text-field");
-    // noinspection ES6ConvertVarToLetConst
     var phoneTextField = $("#phone-text-field");
-    // noinspection ES6ConvertVarToLetConst
     var table = $("#table");
-    // noinspection ES6ConvertVarToLetConst
     var secondNameError = $("#second-name-error-message");
-    // noinspection ES6ConvertVarToLetConst
     var firstNameError = $("#first-name-error-message");
-    // noinspection ES6ConvertVarToLetConst
     var phoneError = $("#phone-error-message");
 
     $("#add-button").click(function () {
-        // noinspection ES6ConvertVarToLetConst
         var secondNameText = secondNameTextField.val();
-        // noinspection ES6ConvertVarToLetConst
         var firstNameText = firstNameTextField.val();
-        // noinspection ES6ConvertVarToLetConst
         var phoneNameText = phoneTextField.val();
-        // noinspection ES6ConvertVarToLetConst
 
         if (secondNameText.trim().length === 0 || firstNameText.trim().length === 0 || phoneNameText.trim().length === 0) {
             if (secondNameText.trim().length === 0) {
                 secondNameError.text("Заполните поле Фамилия");
 
-                secondNameTextField.css({backgroundColor: "red"});
+                secondNameTextField.addClass("background-color-error");
             } else {
                 secondNameError.text("");
 
-                secondNameTextField.css({backgroundColor: "white"});
+                secondNameTextField.removeClass("background-color-error");
             }
 
             if (firstNameText.trim().length === 0) {
                 firstNameError.text("Заполните поле Имя");
 
-                firstNameTextField.css({backgroundColor: "red"});
+                firstNameTextField.addClass("background-color-error");
             } else {
                 firstNameError.text("");
 
-                firstNameTextField.css({backgroundColor: "white"});
+                firstNameTextField.removeClass("background-color-error");
             }
 
             if (phoneNameText.trim().length === 0) {
                 phoneError.text("Заполните поле Номер телефона");
 
-                phoneTextField.css({backgroundColor: "red"});
+                phoneTextField.addClass("background-color-error");
             } else {
                 phoneError.text("");
 
-                phoneTextField.css({backgroundColor: "white"});
+                phoneTextField.removeClass("background-color-error");
             }
 
             return;
@@ -61,14 +50,13 @@ $(document).ready(function () {
         firstNameError.text("");
         phoneError.text("");
 
-        secondNameTextField.css({backgroundColor: "white"});
-        firstNameTextField.css({backgroundColor: "white"});
-        phoneTextField.css({backgroundColor: "white"});
+        secondNameTextField.removeClass("background-color-error");
+        firstNameTextField.removeClass("background-color-error");
+        phoneTextField.removeClass("background-color-error");
 
-        // noinspection ES6ConvertVarToLetConst
         var tableItem = $("<tbody></tbody>");
 
-        tableItem.html("<tr><td><span class = 'index'></span></td><td><span class = 'second-name'></span></td><td><span class = 'first-name'></span></td><td><span class = 'phone'></span></td><td><button class ='delete-button'>X</button></td></tr>");
+        tableItem.html("<tr><td><span class ='index'></span></td><td><span class ='second-name'></span></td><td><span class ='first-name'></span></td><td><span class ='phone'></span></td><td><button class ='delete-button'>X</button></td></tr>");
 
         tableItem.find(".second-name").text(secondNameText);
         tableItem.find(".first-name").text(firstNameText);
