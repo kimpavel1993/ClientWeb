c74f6dd2d52aa804475971ca514301b7a1df17ba
kimpavel1993 <kimpavel1993@mail.ru>
2021-01-12 23:43:19 +0700

    fix task Underscore

--- Task_underscore/script.js
+++ Task_underscore/script.js
@@ -1,101 +1,82 @@
-$(document).ready(function () {
-    var people = [
-        {
-            name: "Pavel",
-            lastName: "Kim",
-            age: 27
-        },
-        {
-            name: "Anna",
-            lastName: "Ivanova",
-            age: 45
-        },
-        {
-            name: "Olga",
-            lastName: "Golubeva",
-            age: 23
-        },
-        {
-            name: "Nikita",
-            lastName: "Kashirin",
-            age: 14
-        },
-        {
-            name: "Alexander",
-            lastName: "Pushkin",
-            age: 27
-        },
-        {
-            name: "Nikolai",
-            lastName: "Vasilkov",
-            age: 52
-        },
-        {
-            name: "Antonina",
-            lastName: "Nosova",
-            age: 20
-        },
-        {
-            name: "Yulia",
-            lastName: "Lobanova",
-            age: 30
-        },
-        {
-            name: "Alexey",
-            lastName: "Fedoseenko",
-            age: 24
-        },
-        {
-            name: "Ekaterina",
-            lastName: "Klimova",
-            age: 21
-        }
-    ];
-
-    var averageAge = {};
-
-    function showPeopleAverageAge(people) {
-        averageAge = _.chain(people)
-            .pluck("age")
-            .map(function (e) {
-                return e / 10;
-            })
-            .reduce(function (sum, e) {
-                return sum + e;
-            }, 0)
-            .value();
+var people = [
+    {
+        name: 'Pavel',
+        lastName: 'Kim',
+        age: 27
+    },
+    {
+        name: 'Anna',
+        lastName: "Ivanova",
+        age: 45
+    },
+    {
+        name: 'Olga',
+        lastName: "Golubeva",
+        age: 23
+    },
+    {
+        name: 'Nikita',
+        lastName: "Kashirin",
+        age: 14
+    },
+    {
+        name: 'Alexander',
+        lastName: 'Pushkin',
+        age: 27
+    },
+    {
+        name: 'Nikolai',
+        lastName: 'Vasilkov',
+        age: 52
+    },
+    {
+        name: 'Antonina',
+        lastName: 'Nosova',
+        age: 20
+    },
+    {
+        name: 'Yulia',
+        lastName: 'Lobanova',
+        age: 30
+    },
+    {
+        name: 'Alexey',
+        lastName: 'Fedoseenko',
+        age: 24
+    },
+    {
+        name: 'Ekaterina',
+        lastName: 'Klimova',
+        age: 21
     }
+];
 
-    showPeopleAverageAge(people);
+function setPeopleAverageAge(people) {
+    return _.chain(people)
+        .pluck('age')
+        .reduce(function (sum, e) {
+            return sum + e / 10;
+        }, 0)
+        .value();
+}
 
-    console.log(averageAge);
+console.log(setPeopleAverageAge(people));
 
-    var filteredPeople = {};
+function setFilteredPeople(people) {
+    return _.chain(people)
+        .filter(function (e) {
+            return e.age >= 20 && e.age <= 30;
+        })
+        .sortBy('age')
+        .value();
+}
 
-    function showFilteredPeople(people) {
-        filteredPeople = _.chain(people)
-            .filter(function (e) {
-                return e.age >= 20 && e.age <= 30;
-            })
-            .sortBy('age')
-            .value();
-    }
+console.log(setFilteredPeople(people));
 
-    showFilteredPeople(people);
+function setFullName() {
+    return people.map(function (e) {
+        return _.extend(e, { fullName: e.name + " " + e.lastName })
+    });
+}
 
-    console.log(filteredPeople);
-
-    var result = {};
-
-    function getFullName(people) {
-        result = _.chain(people)
-            .map(function (e) {
-                return _.extend(e, {fullName: e.name + " " + e.lastName})
-            })
-            .value();
-    }
-
-    getFullName(people);
-
-    console.log(result);
-})
+console.log(setFullName());
