d558e1ee43148cc0574e4e7d6b3963e8489b20f3
kimpavel1993 <kimpavel1993@mail.ru>
2021-01-19 21:58:53 +0700

    fix task Underscore

--- Task_underscore/script.js
+++ Task_underscore/script.js
@@ -1,82 +1,84 @@
-var people = [
-    {
-        name: 'Pavel',
-        lastName: 'Kim',
-        age: 27
-    },
-    {
-        name: 'Anna',
-        lastName: "Ivanova",
-        age: 45
-    },
-    {
-        name: 'Olga',
-        lastName: "Golubeva",
-        age: 23
-    },
-    {
-        name: 'Nikita',
-        lastName: "Kashirin",
-        age: 14
-    },
-    {
-        name: 'Alexander',
-        lastName: 'Pushkin',
-        age: 27
-    },
-    {
-        name: 'Nikolai',
-        lastName: 'Vasilkov',
-        age: 52
-    },
-    {
-        name: 'Antonina',
-        lastName: 'Nosova',
-        age: 20
-    },
-    {
-        name: 'Yulia',
-        lastName: 'Lobanova',
-        age: 30
-    },
-    {
-        name: 'Alexey',
-        lastName: 'Fedoseenko',
-        age: 24
-    },
-    {
-        name: 'Ekaterina',
-        lastName: 'Klimova',
-        age: 21
+(function () {
+    var people = [
+        {
+            name: 'Pavel',
+            lastName: 'Kim',
+            age: 27
+        },
+        {
+            name: 'Anna',
+            lastName: "Ivanova",
+            age: 45
+        },
+        {
+            name: 'Olga',
+            lastName: "Golubeva",
+            age: 23
+        },
+        {
+            name: 'Nikita',
+            lastName: "Kashirin",
+            age: 14
+        },
+        {
+            name: 'Alexander',
+            lastName: 'Pushkin',
+            age: 27
+        },
+        {
+            name: 'Nikolai',
+            lastName: 'Vasilkov',
+            age: 52
+        },
+        {
+            name: 'Antonina',
+            lastName: 'Nosova',
+            age: 20
+        },
+        {
+            name: 'Yulia',
+            lastName: 'Lobanova',
+            age: 30
+        },
+        {
+            name: 'Alexey',
+            lastName: 'Fedoseenko',
+            age: 24
+        },
+        {
+            name: 'Ekaterina',
+            lastName: 'Klimova',
+            age: 21
+        }
+    ];
+
+    function getPeopleAverageAge(people) {
+        return _.chain(people)
+            .pluck('age')
+            .reduce(function (sum, e) {
+                return sum + e / 10;
+            }, 0)
+            .value();
     }
-];
 
-function setPeopleAverageAge(people) {
-    return _.chain(people)
-        .pluck('age')
-        .reduce(function (sum, e) {
-            return sum + e / 10;
-        }, 0)
-        .value();
-}
+    console.log(getPeopleAverageAge(people));
 
-console.log(setPeopleAverageAge(people));
+    function getFilteredPeople(people) {
+        return _.chain(people)
+            .filter(function (e) {
+                return e.age >= 20 && e.age <= 30;
+            })
+            .sortBy('age')
+            .value();
+    }
 
-function setFilteredPeople(people) {
-    return _.chain(people)
-        .filter(function (e) {
-            return e.age >= 20 && e.age <= 30;
-        })
-        .sortBy('age')
-        .value();
-}
+    console.log(getFilteredPeople(people));
 
-console.log(setFilteredPeople(people));
+    function getFullName(people) {
+        return people.map(function (e) {
+            return _.extend(e, {fullName: e.name + ' ' + e.lastName});
+        });
+    }
 
-function setFullName(people) {
-    return people.map(function (e) {
-        return _.extend(e, { fullName: e.name + " " + e.lastName });
-    });
-}
-
-console.log(setFullName(people));
+    console.log(getFullName(people));
+})();
